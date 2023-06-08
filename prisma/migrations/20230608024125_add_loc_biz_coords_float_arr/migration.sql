27c978e89a641b4ede1af9131f542fb43445fa69
Alex Fisher <a.fisher615@gmail.com>
2023-06-08 11:41:57 +0900

    added Float array column to Itinerary_locations and Business_listing for coords

---
+++ prisma/migrations/20230608024125_add_loc_biz_coords_float_arr/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Business_listing" ADD COLUMN     "biz_coords" DOUBLE PRECISION[];
+
+-- AlterTable
+ALTER TABLE "Itinerary_locations" ADD COLUMN     "loc_coords" DOUBLE PRECISION[];
