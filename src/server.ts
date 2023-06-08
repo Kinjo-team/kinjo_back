170c4a7f1286aa42077cf27694cc11ac5e5033ac
Alex Fisher <a.fisher615@gmail.com>
2023-06-08 12:24:03 +0900

    initial commit. Add server config

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,28 @@
-const express = require('express');
-const app = express();
-const PORT = 8000;
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import cors from 'cors'; 
+import { PrismaClient } from '@prisma/client';
 
-app.use(express.static(__dirname + '/public'));
+dotenv.config();
+
+// const express = require('express');
+const app: Express = express();
+const PORT = process.env.PORT || 8000;
+
+// const corsOptions = {
+//     origin: "http://localhost:3000"
+// }
+
+export const prisma = new PrismaClient();
+
+app.use(express.json());
+// app.use(cors(corsOptions));
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, '../public/', 'index.html'));
+})
 
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`);
